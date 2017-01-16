7d4b75d38e4ff226300e485c8e732b30bd85b3e8
Miguel <migueldiniz25@gmail.com>
2017-01-16 11:31:40 +0100

    clase servidor actualizada para que lea los numeros pero no reconoce el operador

--- ServidorSocket/src/servidorsocket/ServidorSocket.java
+++ ServidorSocket/src/servidorsocket/ServidorSocket.java
@@ -1,9 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package servidorsocket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  *
@@ -15,7 +17,44 @@ public class ServidorSocket {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       try{
+			System.out.println("Creando socket servidor");
+	
+			ServerSocket serverSocket=new ServerSocket();
+
+			System.out.println("Realizando el bind");
+
+			InetSocketAddress addr=new InetSocketAddress("localhost",5555);
+			serverSocket.bind(addr);
+
+			System.out.println("Aceptando conexiones");
+
+			Socket newSocket= serverSocket.accept();
+
+			System.out.println("Conexión recibida");
+
+			InputStream is=newSocket.getInputStream();
+			OutputStream os=newSocket.getOutputStream();
+
+                        int num1 = is.read();
+                        int num2= is.read();
+                        byte[] operador= new byte[1];
+                        is.read(operador);
+                        int resultado = (num1+num2);
+			System.out.println("Resultado: "+ resultado);
+
+			System.out.println("Cerrando el nuevo socket");
+
+			newSocket.close();
+
+			System.out.println("Cerrando el socket servidor");
+
+			serverSocket.close();
+
+			System.out.println("Terminado");
+
+			}catch (IOException e) {
+			}
     }
     
 }
