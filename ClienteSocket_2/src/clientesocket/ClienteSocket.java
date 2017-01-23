07723f2e157892c52685cd6aa70b5cd1c75e4889
Miguel <migueldiniz25@gmail.com>
2017-01-23 10:32:34 +0100

    duplicado cliente 1 modificando el puerto

---
+++ ClienteSocket_2/src/clientesocket/ClienteSocket.java
@@ -0,0 +1,64 @@
+package clientesocket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Miguel
+ */
+public class ClienteSocket {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            System.out.println("Creando socket cliente");
+            Socket cliente = new Socket();
+            System.out.println("Estableciendo la conexión");
+
+            InetSocketAddress addr = new InetSocketAddress("localhost", 5556);
+            cliente.connect(addr);
+
+            InputStream is = cliente.getInputStream();
+            OutputStream os = cliente.getOutputStream();
+
+            System.out.println("Enviando mensaje");
+            int num1 = Integer.parseInt(JOptionPane.showInputDialog("Introduce el primer numero"));
+            String petuser = JOptionPane.showInputDialog("Introduce el operador");
+            int num2 = Integer.parseInt(JOptionPane.showInputDialog("Introduce el segundo numero"));
+
+            String operador = "";
+            if (petuser.equalsIgnoreCase("+")) {
+                operador = "sum";
+            } else if (petuser.equalsIgnoreCase("-")) {
+                operador = "res";
+            } else if (petuser.equalsIgnoreCase("/")) {
+                operador = "div";
+            } else if (petuser.equalsIgnoreCase("*")) {
+                operador = "mul";
+            }
+            os.write(num1);
+            os.write(num2);
+            os.write(operador.getBytes());
+
+            System.out.println("Mensaje enviado");
+
+            JOptionPane.showMessageDialog(null, "Resultado= " + is.read());
+            System.out.println("Cerrando el socket cliente");
+
+            cliente.close();
+
+            System.out.println("Terminado");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
