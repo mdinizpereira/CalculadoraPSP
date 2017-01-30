50c3250fb7f89a15bdba36348834aaf676bd5b98
Miguel <migueldiniz25@gmail.com>
2017-01-30 10:41:39 +0100

    añadido javadoc

--- ClienteSocket/src/clientesocket/ClienteSocket.java
+++ ClienteSocket/src/clientesocket/ClienteSocket.java
@@ -18,21 +18,35 @@ public class ClienteSocket {
      */
     public static void main(String[] args) {
         try {
+            /**
+             * se crea el socket
+             */
             System.out.println("Creando socket cliente");
             Socket cliente = new Socket();
             System.out.println("Estableciendo la conexión");
-
+            /**
+             * se define la direccion a la que se tiene que conectar
+             */
             InetSocketAddress addr = new InetSocketAddress("localhost", 5555);
             cliente.connect(addr);
-
+            /**
+             * se crean los flujos del socket
+             */
             InputStream is = cliente.getInputStream();
             OutputStream os = cliente.getOutputStream();
-
+            
+            /**
+             * Por interfaz gráfica de JOptionPane se recogen 2 numeros y el operador
+             */
             System.out.println("Enviando mensaje");
             int num1 = Integer.parseInt(JOptionPane.showInputDialog("Introduce el primer numero"));
             String petuser = JOptionPane.showInputDialog("Introduce el operador");
             int num2 = Integer.parseInt(JOptionPane.showInputDialog("Introduce el segundo numero"));
-
+            /**
+             * según el operador tecleado por el usuario en la variable @petuser
+             * @operador obtendrá un nuevo valor siendo 3 letras que definan la
+             * operación elegida por el usuario
+             */
             String operador = "";
             if (petuser.equalsIgnoreCase("+")) {
                 operador = "sum";
@@ -43,11 +57,21 @@ public class ClienteSocket {
             } else if (petuser.equalsIgnoreCase("*")) {
                 operador = "mul";
             }
+            /**
+             * por el flujo de salida se le envía al servidor primero los 2 numeros
+             * y posteriormente el operador
+             */
             os.write(num1);
             os.write(num2);
             os.write(operador.getBytes());
             System.out.println("Mensaje enviado");
-
+            
+            /**
+             * Después de enviar los datos y que el servidor los haya procesado y enviado
+             * el resultado, el cliente lee por el flujo de entrada lo que le envia
+             * el servidor y lo muestra por JOptionPane.
+             * posteriormente se cierra la conexion
+             */
             JOptionPane.showMessageDialog(null, "Resultado= " + is.read());
             System.out.println("Cerrando el socket cliente");
             cliente.close();
